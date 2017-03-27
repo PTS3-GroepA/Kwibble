cc1de982fb57802f3b935013ce7072fe6db89e0f
Bryan Eggels <356103@student.fontys.nl>
2017-03-27 14:44:47 +0200

    Implemented interface in the context

--- Application/src/Data/Context/Interfaces/PlayerContextInterface.java
+++ Application/src/Data/Context/Interfaces/PlayerContextInterface.java
@@ -1,7 +1,10 @@
 package Data.Context.Interfaces;
 
+import Models.Player;
+
 /**
  * Created by Bryan on 27-3-2017.
  */
 public interface PlayerContextInterface {
+    public boolean AddUser(Player player);
 }
