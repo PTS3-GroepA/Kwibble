668eb96da49a11ea94c408cf72f8a4235a4768af
danny bergh <dandeman111@gmail.com>
2017-04-10 14:45:00 +0200

    Controller geprobeert te fizen maar button werkt niet

--- Application/src/GUI/Controller/MainMenuController.java
+++ Application/src/GUI/Controller/MainMenuController.java
@@ -1,8 +1,31 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Max Meijer on 10/04/2017.
  * Fontys University of Applied Sciences, Eindhoven
  */
-public class MainMenuController {
+public class MainMenuController implements Initializable {
+    @FXML private Button btnJoinGame;
+    @FXML private Button btnHostGame;
+    @FXML private Button btnLocalGame;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        btnHostGame.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                btnHostGame.setText("clicked");
+            }
+        });
+    }
 }
--- Application/src/GUI/Main.java
+++ Application/src/GUI/Main.java
@@ -1,15 +1,9 @@
 package GUI;
 
-import Data.Context.SpotifyContext;
-import Data.Repos.MusicRepository;
-import Models.*;
-import com.wrapper.spotify.models.Track;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 
@@ -31,18 +25,6 @@ public class Main extends Application {
         stage.show();
 
 
-        MusicRepository musicRepo = new MusicRepository(new SpotifyContext());
-        Quiz quiz = new Quiz(10, Difficulty.EASY, "ursulaboy", "4Y9FDNadgIa7n3ktJejcsW");
-        quiz.setAmountOfQuestions(5);
-        quiz.authenticate();
 
-        quiz.generateQuestions();
-        Track track =  musicRepo.getTrack(quiz.getQuestions().get(0).getSource());
-
-
-        Media m = new Media(track.getPreviewUrl());
-        MediaPlayer player = new MediaPlayer(m);
-        player.setVolume(5);
-        player.play();
     }
 }
